144fb9ea946d6e97c5863528a6c3180c790a5d8a
inkyu192 <inkyu192@gmail.com>
2022-09-10 21:17:16 +0900

    상품 저장 DTO 생성

---
+++ src/main/java/com/toy/shop/controller/dto/ItemSaveDTO.java
@@ -0,0 +1,23 @@
+package com.toy.shop.controller.dto;
+
+import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class ItemSaveDTO {
+
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    @Range(min = 100, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9999)
+    private Integer quantity;
+}
