a34fd41046b3ec6f19a487cabb96720d533389af
inkyu192 <inkyu192@gmail.com>
2022-10-13 21:59:17 +0900

    책 조회 API 수정

--- src/main/java/com/toy/shop/controller/dto/BookResponseDto.java
+++ src/main/java/com/toy/shop/controller/dto/BookResponseDto.java
@@ -1,6 +1,7 @@
 package com.toy.shop.controller.dto;
 
 import com.toy.shop.domain.Book;
+import com.toy.shop.domain.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,11 +10,21 @@ public class BookResponseDto {
 
     private Long id;
     private String name;
+    private String description;
+    private String author;
+    private String publisher;
     private int price;
+    private int quantity;
+    private Long categoryId;
 
     public BookResponseDto(Book book) {
         this.id = book.getId();
         this.name = book.getName();
+        this.description = book.getDescription();
+        this.author = book.getAuthor();
+        this.publisher = book.getPublisher();
         this.price = book.getPrice();
+        this.quantity = book.getQuantity();
+        this.categoryId = book.getCategory().getId();
     }
 }
