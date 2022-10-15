0874096ca46c97ce6dadd850939797f0de5473bf
inkyu192 <inkyu192@gmail.com>
2022-10-16 00:07:22 +0900

    CategoryService 추상화

--- src/main/java/com/toy/shop/service/CategoryService.java
+++ src/main/java/com/toy/shop/service/CategoryService.java
@@ -2,33 +2,10 @@ package com.toy.shop.service;
 
 import com.toy.shop.controller.dto.CategoryResponseDto;
 import com.toy.shop.controller.dto.CategorySaveRequestDto;
-import com.toy.shop.domain.Category;
-import com.toy.shop.exception.DataNotFoundException;
-import com.toy.shop.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import static com.toy.shop.common.ResultCode.*;
+public interface CategoryService {
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class CategoryService {
+    CategoryResponseDto save(CategorySaveRequestDto requestDto);
 
-    private final CategoryRepository categoryRepository;
-
-    public CategoryResponseDto save(CategorySaveRequestDto requestDto) {
-        Category category = Category.createCategory(requestDto);
-
-        categoryRepository.save(category);
-
-        return new CategoryResponseDto(category);
-    }
-
-    public CategoryResponseDto findById(Long id) {
-        Category category = categoryRepository.findById(id).orElseThrow(() -> new DataNotFoundException(CATEGORY_NOT_FOUND));
-
-        return new CategoryResponseDto(category);
-    }
+    CategoryResponseDto findById(Long id);
 }
---
+++ src/main/java/com/toy/shop/service/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.toy.shop.service;
+
+import com.toy.shop.controller.dto.CategoryResponseDto;
+import com.toy.shop.controller.dto.CategorySaveRequestDto;
+import com.toy.shop.domain.Category;
+import com.toy.shop.exception.DataNotFoundException;
+import com.toy.shop.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.toy.shop.common.ResultCode.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryResponseDto save(CategorySaveRequestDto requestDto) {
+        Category category = Category.createCategory(requestDto);
+
+        categoryRepository.save(category);
+
+        return new CategoryResponseDto(category);
+    }
+
+    @Override
+    public CategoryResponseDto findById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new DataNotFoundException(CATEGORY_NOT_FOUND));
+
+        return new CategoryResponseDto(category);
+    }
+}
