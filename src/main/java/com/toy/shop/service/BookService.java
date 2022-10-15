6e5bad14fed2251d80659eb13bc0a6556c24ccbf
inkyu192 <inkyu192@gmail.com>
2022-10-15 23:43:37 +0900

    BookService 추상화

--- src/main/java/com/toy/shop/service/BookService.java
+++ src/main/java/com/toy/shop/service/BookService.java
@@ -2,54 +2,14 @@ package com.toy.shop.service;
 
 import com.toy.shop.controller.dto.BookResponseDto;
 import com.toy.shop.controller.dto.BookSaveRequestDto;
-import com.toy.shop.domain.Book;
-import com.toy.shop.domain.Category;
-import com.toy.shop.exception.DataNotFoundException;
-import com.toy.shop.repository.BookQueryRepository;
-import com.toy.shop.repository.BookRepository;
-import com.toy.shop.repository.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static com.toy.shop.common.ResultCode.*;
+public interface BookService {
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class BookService {
+    BookResponseDto save(BookSaveRequestDto requestDto);
 
-    private final BookRepository bookRepository;
-    private final BookQueryRepository bookQueryRepository;
+    BookResponseDto findById(Long id);
 
-    private final CategoryRepository categoryRepository;
-
-    public List<BookResponseDto> findAll(Long categoryId, String searchWord) {
-        List<Book> findBooks = bookQueryRepository.findAll(categoryId, searchWord);
-
-        List<BookResponseDto> collect = findBooks.stream()
-                .map(BookResponseDto::new)
-                .collect(Collectors.toList());
-
-        return collect;
-    }
-
-    public BookResponseDto save(BookSaveRequestDto requestDto) {
-        Category category = categoryRepository.findById(requestDto.getCategoryId()).orElseThrow(() -> new DataNotFoundException(CATEGORY_NOT_FOUND));
-
-        Book book = Book.createBook(requestDto, category);
-
-        bookRepository.save(book);
-
-        return new BookResponseDto(book);
-    }
-
-    public BookResponseDto findById(Long id) {
-        Book book = bookRepository.findById(id).orElseThrow(() -> new DataNotFoundException(BOOK_NOT_FOUND));
-
-        return new BookResponseDto(book);
-    }
+    List<BookResponseDto> findAll(Long categoryId, String searchWord);
 }
---
+++ src/main/java/com/toy/shop/service/BookServiceImpl.java
@@ -0,0 +1,59 @@
+package com.toy.shop.service;
+
+import com.toy.shop.controller.dto.BookResponseDto;
+import com.toy.shop.controller.dto.BookSaveRequestDto;
+import com.toy.shop.domain.Book;
+import com.toy.shop.domain.Category;
+import com.toy.shop.exception.DataNotFoundException;
+import com.toy.shop.repository.BookQueryRepository;
+import com.toy.shop.repository.BookRepository;
+import com.toy.shop.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.toy.shop.common.ResultCode.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    private final BookQueryRepository bookQueryRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public BookResponseDto save(BookSaveRequestDto requestDto) {
+        Category category = categoryRepository.findById(requestDto.getCategoryId()).orElseThrow(() -> new DataNotFoundException(CATEGORY_NOT_FOUND));
+
+        Book book = Book.createBook(requestDto, category);
+
+        bookRepository.save(book);
+
+        return new BookResponseDto(book);
+    }
+
+    @Override
+    public BookResponseDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new DataNotFoundException(BOOK_NOT_FOUND));
+
+        return new BookResponseDto(book);
+    }
+
+    @Override
+    public List<BookResponseDto> findAll(Long categoryId, String searchWord) {
+        List<Book> findBooks = bookQueryRepository.findAll(categoryId, searchWord);
+
+        List<BookResponseDto> collect = findBooks.stream()
+                .map(BookResponseDto::new)
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+}
