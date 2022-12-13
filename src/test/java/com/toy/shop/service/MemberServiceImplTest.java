db6f49c25e067622d21c20435f33fb1e862684cc
inkyu192 <inkyu192@gmail.com>
2022-12-13 23:10:40 +0900

    MemberServiceImplTest findAll

--- src/test/java/com/toy/shop/service/MemberServiceImplTest.java
+++ src/test/java/com/toy/shop/service/MemberServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.toy.shop.service;
 
 import com.toy.shop.domain.Member;
+import com.toy.shop.dto.MemberResponseDto;
 import com.toy.shop.dto.MemberSaveRequestDto;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +43,39 @@ class MemberServiceImplTest {
         Member findMember = entityManager.find(Member.class, saveId);
 
         assertThat(saveId).isEqualTo(findMember.getId());
+    }
+
+    @Test
+    void findAll() {
+        MemberSaveRequestDto requestDto1 = new MemberSaveRequestDto();
+        requestDto1.setName("박인규");
+        requestDto1.setCity("화성");
+        requestDto1.setStreet("순환대로");
+        requestDto1.setZipcode("000");
+
+        Member saveMember1 = Member.createMember(requestDto1);
+
+        entityManager.persist(saveMember1);
+
+        MemberSaveRequestDto requestDto2 = new MemberSaveRequestDto();
+        requestDto2.setName("박인규");
+        requestDto2.setCity("화성");
+        requestDto2.setStreet("순환대로");
+        requestDto2.setZipcode("000");
+
+        Member saveMember2 = Member.createMember(requestDto2);
+
+        entityManager.persist(saveMember2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        List<MemberResponseDto> list = memberService.findAll(null);
+
+        List<Long> collect = list.stream()
+                .map(MemberResponseDto::getId)
+                .toList();
+
+        assertThat(collect).containsOnly(saveMember1.getId(), saveMember2.getId());
     }
 }
