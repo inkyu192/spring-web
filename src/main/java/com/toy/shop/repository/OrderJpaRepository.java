3bf40e2da44be9ad47f03ea7572ae23d609671f4
inkyu192 <inkyu192@gmail.com>
2022-12-25 23:37:48 +0900

    OrderJpaRepository 생성

---
+++ src/main/java/com/toy/shop/repository/OrderJpaRepository.java
@@ -0,0 +1,22 @@
+package com.toy.shop.repository;
+
+import com.toy.shop.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderJpaRepository {
+
+    private final EntityManager entityManager;
+
+    public Order save(Order order) {
+        if (order.getId() == null) {
+            entityManager.persist(order);
+            return order;
+        } else {
+            return entityManager.merge(order);
+        }
+    }
+}
