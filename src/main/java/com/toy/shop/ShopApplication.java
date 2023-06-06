bc12d3c9c0a7dd4d03b74a63672ae40001a421da
inkyu192 <inkyu192@gmail.com>
2023-06-06 22:35:38 +0900

    ApplicationRunner 수정

--- src/main/java/com/toy/shop/ShopApplication.java
+++ src/main/java/com/toy/shop/ShopApplication.java
@@ -1,30 +1,15 @@
 package com.toy.shop;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 public class ShopApplication {
-	@Bean
-	public ApplicationRunner applicationRunner(ApplicationContext applicationContext) {
-		return args -> {
-			String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
 
-			for (String beanDefinitionName : beanDefinitionNames) {
-				Object bean = applicationContext.getBean(beanDefinitionName);
-				log.info("name: {}, object: {}", beanDefinitionName, bean);
-			}
-		};
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 }
