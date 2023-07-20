61f362b4dceaa7c80bad504c5588678fb3cbb74d
inkyu192 <inkyu192@gmail.com>
2023-07-21 00:08:52 +0900

    정리

--- src/main/java/com/toy/shop/common/Constants.java
+++ src/main/java/com/toy/shop/common/Constants.java
@@ -1,8 +1,5 @@
 package com.toy.shop.common;
 
-/**
- * name()은 final 메서드이므로 재정의할 수 없지만 toString()은 재정의가 가능하다.
- */
 public enum Constants {
 
     LOG_ID
--- src/main/java/com/toy/shop/config/ServletConfig.java
+++ src/main/java/com/toy/shop/config/ServletConfig.java
@@ -7,11 +7,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * {@code @Component} 방식은 {@code @Order}로 순서는 지정할 수 있으나 url 패턴 매핑이 불가
- * <p>{@code @ServletComponentScan} + {@code @WebFilter} 방식은 url 패턴 매핑은 가능하나 순서 지정 불가
- * <p>{@code FilterRegistrationBean} 방식은 url 패턴 매핑, 순서 지정 둘다 가능
- */
 @Configuration(proxyBeanMethods = false)
 //@ServletComponentScan(basePackages = "com.toy.shop.filter")
 public class ServletConfig {
