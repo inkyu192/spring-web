5e3aa4af58a6dfd725c30aaed8b8bac9f665ebcf
inkyu192 <inkyu192@gmail.com>
2023-08-04 00:19:44 +0900

    초기 데이터 생성 ApplicationRunner

---
+++ src/main/java/com/toy/shop/common/DataInitApplicationRunner.java
@@ -0,0 +1,22 @@
+package com.toy.shop.common;
+
+import com.toy.shop.business.role.dto.request.RoleSaveRequest;
+import com.toy.shop.business.role.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataInitApplicationRunner implements ApplicationRunner {
+
+    private final RoleService roleService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        roleService.saveRole(new RoleSaveRequest("ROLE_MEMBER", "사용자 권한", "사용자 권한"));
+    }
+}
