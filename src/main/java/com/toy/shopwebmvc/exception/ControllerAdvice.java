99dfa611cd1aef52b4db6597a4a6aecb31b2da7f
inkyu192 <inkyu192@gmail.com>
2023-12-18 00:41:18 +0900

    수정

--- src/main/java/com/toy/shopwebmvc/exception/ControllerAdvice.java
+++ src/main/java/com/toy/shopwebmvc/exception/ControllerAdvice.java
@@ -2,7 +2,9 @@ package com.toy.shopwebmvc.exception;
 
 import com.toy.shopwebmvc.constant.ApiResponseCode;
 import com.toy.shopwebmvc.dto.response.ApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
-
 
 
 @Slf4j
@@ -36,9 +37,14 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResponse<Void> handler(JwtException e) {
-        return new ApiResponse<>(ApiResponseCode.BAD_REQUEST, e.getMessage());
+        if (e instanceof UnsupportedJwtException) {
+            return new ApiResponse<>(ApiResponseCode.UNSUPPORTED_TOKEN);
+        } else if (e instanceof ExpiredJwtException) {
+            return new ApiResponse<>(ApiResponseCode.EXPIRED_TOKEN);
+        } else{
+            return new ApiResponse<>(ApiResponseCode.BAD_TOKEN);
+        }
     }
 
     @ExceptionHandler
