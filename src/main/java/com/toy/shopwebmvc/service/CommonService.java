761a64b367fe49199e407c76178c750b1f29afa4
inkyu192 <inkyu192@gmail.com>
2023-12-17 20:24:49 +0900

    수정

--- src/main/java/com/toy/shopwebmvc/service/CommonService.java
+++ src/main/java/com/toy/shopwebmvc/service/CommonService.java
@@ -1,11 +1,14 @@
 package com.toy.shopwebmvc.service;
 
 import com.toy.shopwebmvc.config.security.JwtTokenProvider;
+import com.toy.shopwebmvc.constant.ApiResponseCode;
 import com.toy.shopwebmvc.dto.request.LoginRequest;
 import com.toy.shopwebmvc.dto.response.TokenResponse;
+import com.toy.shopwebmvc.exception.CommonException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -22,8 +25,13 @@ public class CommonService {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                 new UsernamePasswordAuthenticationToken(loginRequest.account(), loginRequest.password());
 
-        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        Authentication authentication;
 
+        try {
+            authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new CommonException(ApiResponseCode.BAD_CREDENTIALS);
+        }
 
         return new TokenResponse(jwtTokenProvider.createAccessToken(authentication));
     }
