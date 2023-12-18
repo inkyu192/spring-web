83a758d9026202bf1a3ded448faebaacb9e8c1d5
inkyu192 <inkyu192@gmail.com>
2023-12-19 01:15:08 +0900

    수정

--- src/main/java/com/toy/shopwebmvc/config/security/JwtExceptionFilter.java
+++ src/main/java/com/toy/shopwebmvc/config/security/JwtExceptionFilter.java
@@ -3,38 +3,40 @@ package com.toy.shopwebmvc.config.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.toy.shopwebmvc.constant.ApiResponseCode;
 import com.toy.shopwebmvc.dto.response.ApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
 
 @RequiredArgsConstructor
-public class JwtExceptionFilter extends OncePerRequestFilter {
+public class JwtExceptionFilter extends GenericFilterBean {
 
     private final ObjectMapper objectMapper;
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-            filterChain.doFilter(request, response);
+            chain.doFilter(request, response);
+        } catch (UnsupportedJwtException e) {
+            setResponse(response, ApiResponseCode.UNSUPPORTED_TOKEN);
+        } catch (ExpiredJwtException e) {
+            setResponse(response, ApiResponseCode.EXPIRED_TOKEN);
         } catch (JwtException e) {
-            setResponse(response, e);
+            setResponse(response, ApiResponseCode.BAD_TOKEN);
         }
     }
 
-    private void setResponse(HttpServletResponse response, JwtException e) throws RuntimeException, IOException {
-        String result = objectMapper.writeValueAsString(new ApiResponse<>(ApiResponseCode.BAD_REQUEST, e.getMessage()));
+    private void setResponse(ServletResponse response, ApiResponseCode apiResponseCode) throws RuntimeException, IOException {
+        String result = objectMapper.writeValueAsString(new ApiResponse<>(apiResponseCode));
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
