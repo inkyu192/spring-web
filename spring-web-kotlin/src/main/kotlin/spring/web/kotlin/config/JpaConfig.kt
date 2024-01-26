20fee80cf0f029a15d8676f540085c359a57c7a2
inkyu192 <inkyu192@gmail.com>
2024-01-26 15:23:58 +0900

    수정

---
+++ spring-web-kotlin/src/main/kotlin/spring/web/kotlin/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package spring.web.kotlin.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration(proxyBeanMethods = false)
+class JpaConfig
