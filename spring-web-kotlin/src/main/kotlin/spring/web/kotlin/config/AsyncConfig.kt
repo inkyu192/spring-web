61245136671ebdf6c54096c7db9e3e192aee5386
inkyu192 <inkyu192@gmail.com>
2024-02-13 11:53:57 +0900

    수정

---
+++ spring-web-kotlin/src/main/kotlin/spring/web/kotlin/config/AsyncConfig.kt
@@ -0,0 +1,20 @@
+package spring.web.kotlin.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+@EnableAsync
+@Configuration(proxyBeanMethods = false)
+class AsyncConfig {
+
+    @Bean
+    fun threadPoolTaskExecutor() = ThreadPoolTaskExecutor()
+        .apply {
+            corePoolSize = 3
+            maxPoolSize = 10
+            queueCapacity = 100
+            setThreadNamePrefix("Executor-")
+        }
+}
