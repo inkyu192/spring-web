79a9fac7df591b7ec7e653c1fcb71be71f45e8ca
inkyu192 <inkyu192@gmail.com>
2024-03-18 10:54:52 +0900

    수정

--- spring-web-java/src/main/java/spring/web/java/exception/ControllerAdvice.java
+++ spring-web-java/src/main/java/spring/web/java/exception/ControllerAdvice.java
@@ -1,8 +1,7 @@
 package spring.web.java.exception;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -16,45 +15,42 @@ import spring.web.java.dto.response.ApiResponse;
 import java.util.List;
 
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler
-    public ApiResponse<Void> handler(CommonException commonException) {
-        return new ApiResponse<>(commonException.getApiResponseCode());
+    public ApiResponse<Void> handler(CommonException e) {
+        return new ApiResponse<>(e.getApiResponseCode());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResponse<List<String>> handler(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException) {
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ApiResponse<List<String>> handler(HttpRequestMethodNotSupportedException e) {
         return new ApiResponse<>(ApiResponseCode.BAD_REQUEST);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResponse<List<String>> handler(MethodArgumentNotValidException methodArgumentNotValidException) {
+    public ApiResponse<List<String>> handler(MethodArgumentNotValidException e) {
         return new ApiResponse<>(
                 ApiResponseCode.PARAMETER_NOT_VALID,
-                methodArgumentNotValidException.getBindingResult().getFieldErrors().stream()
+                e.getBindingResult().getFieldErrors().stream()
                         .map(FieldError::getField)
                         .toList()
         );
     }
 
     @ExceptionHandler
-    public ApiResponse<Void> handler(JwtException jwtException) {
-        if (jwtException instanceof UnsupportedJwtException) {
-            return new ApiResponse<>(ApiResponseCode.UNSUPPORTED_TOKEN);
-        } else if (jwtException instanceof ExpiredJwtException) {
-            return new ApiResponse<>(ApiResponseCode.EXPIRED_TOKEN);
-        } else {
-            return new ApiResponse<>(ApiResponseCode.BAD_TOKEN);
-        }
+    public ApiResponse<Void> handler(JwtException e) {
+        throw e;
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResponse<Void> handler(Exception exception) {
+    public ApiResponse<Void> handler(Exception e) {
+        log.error("[ExceptionHandler]", e);
+
         return new ApiResponse<>(ApiResponseCode.SYSTEM_ERROR);
     }
 }
--- spring-web-kotlin/src/main/kotlin/spring/web/kotlin/exception/ControllerAdvice.kt
+++ spring-web-kotlin/src/main/kotlin/spring/web/kotlin/exception/ControllerAdvice.kt
@@ -1,12 +1,12 @@
 package spring.web.kotlin.exception
 
-import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtException
-import io.jsonwebtoken.UnsupportedJwtException
+import org.springframework.http.HttpStatus
 import org.springframework.validation.FieldError
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import spring.web.kotlin.constant.ApiResponseCode
 import spring.web.kotlin.dto.response.ApiResponse
@@ -18,21 +18,18 @@ class ControllerAdvice {
     fun handler(commonException: CommonException) = ApiResponse<Void>(commonException.apiResponseCode)
 
     @ExceptionHandler
-    fun handler(httpRequestMethodNotSupportedException: HttpRequestMethodNotSupportedException) =
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    fun handler(e: HttpRequestMethodNotSupportedException) =
         ApiResponse<Void>(ApiResponseCode.BAD_REQUEST)
 
     @ExceptionHandler
-    fun handler(methodArgumentNotValidException: MethodArgumentNotValidException) = ApiResponse(
+    fun handler(e: MethodArgumentNotValidException) = ApiResponse(
         ApiResponseCode.PARAMETER_NOT_VALID,
-        methodArgumentNotValidException.bindingResult.fieldErrors.map(FieldError::getField)
+        e.bindingResult.fieldErrors.map(FieldError::getField)
     )
 
     @ExceptionHandler
-    fun handler(jwtException: JwtException): ApiResponse<ApiResponseCode> = when (jwtException) {
-        is UnsupportedJwtException -> ApiResponse(ApiResponseCode.UNSUPPORTED_TOKEN)
-        is ExpiredJwtException -> ApiResponse(ApiResponseCode.EXPIRED_TOKEN)
-        else -> ApiResponse(ApiResponseCode.BAD_TOKEN)
-    }
+    fun handler(e: JwtException): ApiResponse<ApiResponseCode> = throw e
 
     @ExceptionHandler
     fun handler(exception: Exception) = ApiResponse<Void>(ApiResponseCode.SYSTEM_ERROR)
