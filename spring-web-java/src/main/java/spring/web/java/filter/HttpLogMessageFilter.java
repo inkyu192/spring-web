3a897d1f1d13bd49bf4ef912671bfded5905c2f9
inkyu192 <inkyu192@gmail.com>
2024-06-06 17:56:19 +0900

    수정

---
+++ spring-web-java/src/main/java/spring/web/java/filter/HttpLogMessageFilter.java
@@ -0,0 +1,45 @@
+package spring.web.java.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import spring.web.java.dto.HttpLogMessage;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class HttpLogMessageFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String transactionId = UUID.randomUUID().toString();
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+        servletRequest.setAttribute("transactionId", transactionId);
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long endTime = System.currentTimeMillis();
+
+        HttpLogMessage httpLogMessage = new HttpLogMessage(
+                transactionId,
+                requestWrapper,
+                responseWrapper,
+                (endTime - startTime) / 1000.0
+        );
+        log.info(httpLogMessage.getMessage());
+
+        responseWrapper.copyBodyToResponse();
+    }
+}
