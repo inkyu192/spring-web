9b30497e94eaa9f873114fd673b17863d25bdfd5
inkyu192 <inkyu192@gmail.com>
2024-10-24 16:52:38 +0900

    restdocs 추가

---
+++ spring-web-java/src/main/java/spring/web/java/controller/TestController.java
@@ -0,0 +1,18 @@
+package spring.web.java.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/example")
+@RequiredArgsConstructor
+public class TestController {
+
+	@GetMapping
+	public String getExample() {
+		return "Hello, Spring REST Docs!";
+	}
+}
