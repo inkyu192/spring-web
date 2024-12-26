39097a19b1212f75b696fac438bff60854aa3477
inkyu192 <inkyu192@gmail.com>
2024-12-26 10:37:14 +0900

    ControllerAdvice ProblemDetail 적용

--- spring-web-java/src/main/java/spring/web/java/infrastructure/configuration/exception/ControllerAdvice.java
+++ spring-web-java/src/main/java/spring/web/java/infrastructure/configuration/exception/ControllerAdvice.java
@@ -1,21 +1,17 @@
 package spring.web.java.infrastructure.configuration.exception;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.ServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import spring.web.java.common.ApiResponseCode;
-import spring.web.java.dto.response.ApiResponse;
 import spring.web.java.infrastructure.configuration.filter.HttpLogFilter;
 
 @Slf4j
@@ -26,37 +22,46 @@ public class ControllerAdvice {
 	private final ServletRequest servletRequest;
 
 	@ExceptionHandler
-	public ApiResponse<Void> handler(CommonException e) {
-		return new ApiResponse<>(e.getApiResponseCode());
+	public ProblemDetail handler(HttpRequestMethodNotSupportedException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.METHOD_NOT_ALLOWED);
+
+		problemDetail.setTitle(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
+		problemDetail.setDetail(e.getMessage());
+
+		return problemDetail;
 	}
 
 	@ExceptionHandler
-	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-	public ApiResponse<List<String>> handler(HttpRequestMethodNotSupportedException e) {
-		return new ApiResponse<>(ApiResponseCode.BAD_REQUEST);
+	public ProblemDetail handler(MethodArgumentNotValidException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+		problemDetail.setTitle(HttpStatus.BAD_REQUEST.getReasonPhrase());
+		problemDetail.setDetail(e.getMessage() + ": " + e.getBindingResult().getFieldErrors().stream()
+			.map(FieldError::getField)
+			.toList());
+
+		return problemDetail;
 	}
 
 	@ExceptionHandler
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiResponse<List<String>> handler(MethodArgumentNotValidException e) {
-		return new ApiResponse<>(
-			ApiResponseCode.PARAMETER_NOT_VALID,
-			e.getBindingResult().getFieldErrors().stream()
-				.map(FieldError::getField)
-				.toList()
-		);
+	public ProblemDetail handler(JwtException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+
+		problemDetail.setTitle(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+		problemDetail.setDetail(e.getMessage());
+
+		return problemDetail;
 	}
 
 	@ExceptionHandler
-	public ApiResponse<Void> handler(JwtException e) {
-		throw e;
-	}
-
-	@ExceptionHandler
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public ApiResponse<Void> handler(Exception e) {
+	public ProblemDetail handler(Exception e) {
 		log.error("[{}]", servletRequest.getAttribute(HttpLogFilter.TRANSACTION_ID), e);
 
-		return new ApiResponse<>(ApiResponseCode.SYSTEM_ERROR);
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+		problemDetail.setTitle(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		problemDetail.setDetail(e.getMessage());
+
+		return problemDetail;
 	}
 }
