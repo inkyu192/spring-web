c044e1b679c9727d091c1f71b2a7f7497aa234f3
inkyu192 <inkyu192@gmail.com>
2025-01-22 16:01:21 +0900

    테스트 라이브러리 추가

--- spring-web-kotlin/build.gradle.kts
+++ spring-web-kotlin/build.gradle.kts
@@ -40,9 +40,10 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+    testImplementation("io.mockk:mockk:1.13.16")
 }
 
 kotlin {
