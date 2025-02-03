9ed550327d1ec21fa9eb8e4efa9bb434e4764d2a
inkyu192 <inkyu192@gmail.com>
2025-02-03 18:43:19 +0900

    SecurityUtils abstract class -> object 수정

--- spring-web-kotlin/src/main/kotlin/spring/web/kotlin/global/common/SecurityUtils.kt
+++ spring-web-kotlin/src/main/kotlin/spring/web/kotlin/global/common/SecurityUtils.kt
@@ -2,11 +2,9 @@ package spring.web.kotlin.global.common
 
 import org.springframework.security.core.context.SecurityContextHolder
 
-abstract class SecurityUtils {
-    companion object {
-        fun getMemberId(): Long? {
-            val principal = SecurityContextHolder.getContext().authentication?.principal
-            return principal.toString().toLongOrNull()
-        }
+object SecurityUtils {
+    fun getMemberId(): Long? {
+        val principal = SecurityContextHolder.getContext().authentication?.principal
+        return principal.toString().toLongOrNull()
     }
 }
