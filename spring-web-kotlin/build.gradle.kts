549c8a13bcb129958c7dad61f05b9588e7058510
inkyu192 <inkyu192@gmail.com>
2025-02-06 17:42:13 +0900

    plugins 정리

--- spring-web-kotlin/build.gradle.kts
+++ spring-web-kotlin/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.7"
-    id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
     kotlin("kapt")  version "1.9.25"
+    id("org.springframework.boot") version "3.3.7"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
