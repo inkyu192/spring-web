d30e0c040bc2e9ab77788f47f611468853063f8d
inkyu192 <inkyu192@gmail.com>
2025-03-15 20:01:15 +0900

    설정 정리

--- spring-web-kotlin/build.gradle.kts
+++ spring-web-kotlin/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
